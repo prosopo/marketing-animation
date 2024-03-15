d745183d6d2eb08a470b1b981aca3b3ec9771e01
Jalpa Rajodiya <dev.jd28491@gmail.com>
2024-03-15 14:42:52 +0530

    fix: resolved  mobile scroll issue and ui issues

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,4 +1,3 @@
-const PROGRESS_STEPS = 200
 const EVENT_DEFS = [
     'click',
     'touchstart',
@@ -32,11 +31,13 @@ const EVENT_DEFS = [
 console.log(EVENT_DEFS)
 
 document.addEventListener('DOMContentLoaded', () => {
+    resetScroll({ duration: 500 });
     for (const eventName of EVENT_DEFS) {
         console.log("adding event for ", eventName)
         addEventListener(eventName, handleEvent)
     }
-    window.progressBarMax = getMaxWidth()
+    window.prevWidth = window.innerWidth;
+    window.progressSteps = window.innerWidth > 1000 ? 400 : 200; // set progress step according devices
 });
 
 // reset the progress bar when the window size changes.
@@ -46,14 +47,24 @@ window.addEventListener("resize", () => {
     
     // if the current screen size does not match with the previous screen size
     if (currentWidth !== previousWidth) {
+        window.progressSteps = window.innerWidth > 700 ? 400 : 200;
+        resetScroll({ duration: 1000 });
         getProgressWidth(); // set the new max width for the progress bar
-        window.progressBar = 0; // reset the width of the progress bar
+        window.progressBar = '0'; // reset the width of the progress bar
         window.prevWidth = window.innerWidth; // update the previous screen size
 
         const progressElement = getProgressBar(); // get the progress bar element
         progressElement.removeAttribute("style"); // removed progress bar styles
     }
 });
+
+const resetScroll = ({ duration = 1000 }) => {
+    window.resizeScrolling = true;  // set the flag to true when resizing triggers scrolling
+    window.scroll({ top: 0, behavior: 'smooth' });
+    setTimeout(() => {
+        window.resizeScrolling = false;
+    }, duration);
+}
 
 // create a common function to retrieve the maximum width of the progress bar
 const getProgressWidth = () => {
@@ -65,7 +76,7 @@ const getProgressWidth = () => {
 }
 
 const getIncrement = (maxWidth) => {
-    return Math.floor(maxWidth / PROGRESS_STEPS)
+    return Math.floor(maxWidth / window.progressSteps)
 }
 
 const getMaxWidth = () => {
@@ -81,14 +92,18 @@ const getProgressBar = () => {
 
 const handleEvent = () => {
     console.log("event!")
-    if (!window.progressBarFinished) {
+    if (!window.progressBarFinished && !window.resizeScrolling) {
         const elem = getProgressBar()
         const width = window.progressBar || getComputedStyle(elem).width.replace(/px.*/, '')
         const maxWidth = getMaxWidth()
         const newWidth = Number(width) + getIncrement(maxWidth)
+        // get checkbox element
+        const getCheckboxElement = document.getElementById("procaptchaCheckbox");
+
         if (newWidth > maxWidth) {
             // enable the checkbox when the progress bar is not progressing
-            document.getElementById("procaptchaCheckbox").disabled = false;
+            getCheckboxElement.disabled = false;
+            getCheckboxElement.style.cursor = "pointer";
             
             window.checkBox()
             window.progressBarFinished = true
@@ -99,10 +114,12 @@ const handleEvent = () => {
             removeEventListeners()
         } else {
             // disable the checkbox while the progress bar is in progress
-            document.getElementById("procaptchaCheckbox").disabled = true;
+            getCheckboxElement.disabled = true;
+            getCheckboxElement.style.cursor = "not-allowed";
+            getCheckboxElement.style.backgroundColor = "rgb(227 227 227)";
 
             window.progressBar = newWidth
-            const newStyle = { ...elem.style, width: `${newWidth}px`, height: '128px' }
+            const newStyle = { ...elem.style, width: `${newWidth}px`, height: '124px', border: '2px solid transparent', 'border-radius': '8px' }
             //remove empty style properties
             Object.keys(newStyle).forEach((key) => {
                 if (!newStyle[key]) {
@@ -125,8 +142,8 @@ const removeEventListeners = () => {
     }
     const progressElement = getProgressBar()
 
-    progressElement.classList.add('animate-fade')
-    progressElement.style.backgroundColor = 'white'
+    progressElement.style.backgroundColor = 'white';
+    progressElement.style.width = '100%';
 
     const demoSection = document.getElementById('demo-section')
     demoSection.classList.add('animate-fade')
